a680433f90104545863bb9b076cafa7a18f92909
F Bliss <vanavil@fastmail.com>
2022-11-04 10:20:28 +0800

    Template Literals complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-alert(123);
+// alert(123);
 
 const name = 'John';
-const age = 32;
+const age = 29;
 const job = 'Co-Founder';
 const city = 'Auckland';
 
@@ -10,6 +10,31 @@ let html;
 // Without template strings (es5)
 // html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
 
-html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: '+ job+ ' </li><li>City: '+ city +' </li></ul>';
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: '+ job+ ' </li><li>City: '+ city +' </li></ul>';
+
+// html = '<ul>' + 
+//         '<li>Name: '+ name + '</li>' +
+//         '<li>Name: '+ age + '</li>' +
+//         '<li>Name: '+ job + '</li>' +
+//         '<li>Name: '+ city + '</li>' +
+//         '</ul>';
+
+//  With template strings
+
+function hello(){
+    return 'hello';
+}
+
+html = `
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+        <li>${2 + 2}</li>
+        <li>${hello()}</li>
+        <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+    </ul>
+`;
 
 document.body.innerHTML = html;
