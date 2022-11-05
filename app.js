7d69ed721dd388460880c448d0e4e54effa53e3e
F Bliss <vanavil@fastmail.com>
2022-11-05 18:34:25 +0800

    arrays completed

--- app.js
+++ app.js
@@ -6,8 +6,53 @@ const mixed = new Array(22, 'Hello', true, undefined, null, {a:2, b:1}, new Date
 
 let val;
 
-// Get array length
-val =
+// // Get array length
+// val = numbers.length;
+// // Check if is array
+// val = Array.isArray(numbers);
+// // Check if is array
+// val = Array.isArray('hello');
+// // Get single value
+// val = numbers[3];
+// val = numbers[0];
+// // Insert into array
+// numbers[2] = '100';
+// // Find index of value
+// val = numbers.indexOf(36);
+
+// // MUTATING ARRAYS
+// // Add to end
+// numbers.push(250);
+// // Add to front
+// numbers.unshift(120);
+// // Take off from end
+// numbers.pop();
+// // Take off from front
+// numbers.shift();
+// // Splice values
+// numbers.splice(1, 3);
+// // Reverse
+// numbers.reverse();
+// // Concatenate arrays
+// val = numbers.concat(numbers2);
+// // Sorting arrays
+// val = fruit.sort();
+// val = numbers.sort();
+// Use the "compare function"
+val = numbers.sort(function(x, y) {
+   return  x - y;
+});
+
+// Reverse sort
+val = numbers.sort(function(x, y) {
+    return y - x;
+});
+
+// Find
+function over50(num){
+    return num > 50;
+}
+val = numbers.find(over50);
 
 console.log(numbers);
 console.log(val);
