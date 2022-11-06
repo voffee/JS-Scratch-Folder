d184ac5bea04ae30e268295ddee17f84826664e0
F Bliss <vanavil@fastmail.com>
2022-11-06 13:22:46 +0800

    Dates JS Completed

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-const person = {
-    firstName: 'Steve',
-    lastName: 'Smith',
-    age: 30,
-    email: 'steve@aol.com',
-    hobbies: ['music', 'sports'],
-    address: {
-        city: 'Boston',
-        state: 'Massachusetts'
-    },
-    getBirthYear: function() {
-        return 2022 - this.age;
-    }
-}
-
 let val;
 
-val = person;
-// Get specific value
-val = person.firstName;
-val = person.age;
-val = person.hobbies[1];
-val = person.address.state;
-val = person.getBirthYear();
-console.log(val);
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00');
+// Multiple ways to set date, please read documentation for date
+birthday = new Date('December 13 1921');
 
-const people = [
-    {name: 'John', age: 30},
-    {name: 'Mike', age: 29},
-    {name: 'Nancy', age: 55}
+// getMonth method is 0 based, so 10 = November since - is January and this file was created in November 2022
 
-];
+val = today.getMonth();
+val = today.getDate();
+// getDay method gives numerical number of the day, starting from 0 = Sunday
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+// getTime is timestamp
+val = today.getTime();
 
-for(let i = 0; i < people.length; i++){
-    console.log(people[i].name);
-}
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(1991);
+birthday.setHours(3);
+birthday.setMinutes(20);
+birthday.setSeconds(10);
+
+console.log(birthday);
