085b8ab1f73018a130e618624f9de678cd119138
F Bliss <vanavil@fastmail.com>
2022-11-07 17:58:57 +0800

    Switch Case Partial

--- app.js
+++ app.js
@@ -1,88 +1,14 @@
-// if (something){
-//         do something
-// } 
-// else {
-//     do something else
-// }
-
-const id = 100;
-
-// // EQUAL TO
-// if(id == 101){
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// // NOT EQUAL TO
-// if(id != 101){
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// // EQUAL TO VALUE & TYPE
-// // Use triple equal by default
-// if(id === 101){
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// // NOT EQUAL TO VALUE & TYPE
-// if(id !== 100){
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// Test to check if variable is undefined
-// if(typeof id !== 'undefined'){
-//     console.log(`The ID is ${id}`);
-// }   else {
-//     console.log('NO ID');
-// }
-
-// Greater or Less Than
-// if(id > 200){
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// IF ELSE
 const color = 'yellow';
 
-// if(color === 'red') {
-//     console.log('Color is red');
-// } else if(color === 'blue') {
-//     console.log('Color is blue');
-// } else {
-//     console.log('Color is not red or blue');
-// }
+switch(color){
+    case 'red': 
+        console.log('Color is red');
+        break;
+    case 'blue':
+        console.log('Color is blue');
+        break;
 
-// LOGICAL OPERATORS
-
-const name = 'Steve';
-const age = 44;
-
-// AND &&
-// if(age > 0 && age <= 12){
-//     console.log(`${name} is a child`);
-// } else if (age >= 13 && age <= 19) {
-//     console.log(`${name} is a teenager`);
-// } else {
-//     console.log(`${name} is an adult`);
-// }
-
-// OR ||
-if(age < 16 || age > 65){
-    console.log(`${name} can not run in race`);
-// } else if (age >= 13 && age <= 19) {
-//     console.log(`${name} is a teenager`);
-} else {
-    console.log(`${name} is registered for the race`);
+    default:
+        console.log('Color is not red or blue');
+        break;
 }
-
-// TERNARY OPERATOR ? :
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
