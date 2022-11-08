e03c1f70a3f751ecbed0625cf69214122ee51bbf
F Bliss <vanavil@fastmail.com>
2022-11-08 18:27:08 +0800

    Loops JS Partial

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-// Function Declarations
+// General Loops
+// For, While, Do-While
 
-function greet(firstName = 'John', lastName = 'Doe'){
-    // if(typeof firstName === 'undefined'){firstName = 'John'}
-    // if(typeof lastName === 'undefined'){lastName = 'Doe'}
-    // console.log('Hello');
-    return 'Hello '+ firstName + ' ' + lastName;
-}
-
-// console.log(greet('Steve', 'Jobs'));
-
-// Function Expressions
-
-const square = function(x = 3){
-    return x * x;
-};
-
-// console.log(square());
-
-// Immediately Invokable FUnctions Expressions - IIFEs
-// (function(){
-//     console.log('IFFE RAN..');
-// })();
-
-// (function(name){
-//     console.log('Hello ' + name);
-// })('Bob');
-
-// Property Methods
-
-const todo = {
-    add: function(){
-        console.log('Add todo..');
-    },
-    edit: function(id){
-        console.log(`Edit todo ${id}`);
+for(let i = 0; i <= 10; i++){
+    // console.log(`Hello World ${i}`);
+    if(i === 2){
+        console.log('2 is my favorite number');
+        continue;
     }
+    console.log('Number ' + i);
 }
-
-todo.delete = function(){
-    console.log(`Delete todo...`);
-}
-
-todo.add();
-todo.edit(22);
-todo.delete();
-
-// I DON'T UNDERSTAND PROPERTY METHODS!!
