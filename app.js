176ee5da467f4aeb96ffce96e0ce2650eaeeaa84
F Bliss <vanavil@fastmail.com>
2022-11-19 01:35:48 +0800

    DOM Multiple Selectors Partial

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
-// document.getElementById()
+// document.detElementsByClassName
+// this way of pulling is global
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-console.log(document.getElementById('task-title'));
+// // Below method is only pulling from ul and not globally pulling 'collection-items'
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-// Get things from the element
+// console.log(listItems);
 
-console.log(document.getElementById(('task-title')).id);
-console.log(document.getElementById(('task-title')).className);
-
-
-// Store the method call in a variable and use that to change things
-const taskTitle = document.getElementById('task-title');
-
-// Change styling
-taskTitle.style.background = '#333333';
-taskTitle.style.color = '#ffffff';
-taskTitle.style.padding = '5px';
-// document.getElementById('task-title').style.display = 'none';
-
-// Change content
-taskTitle.textContent = 'Task List';
-taskTitle.innerText = 'My Tasks';
-
-// Insert HTML
-taskTitle.innerHTML = '<span style = "color: red">Task List</span>';
-
-// document.querySelector()
-
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
-
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
-
-document.querySelector('li:last-child').style.color = 'yellow';
-document.querySelector('li:nth-child(3)').style.color = 'green';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-
-// Only works with 1st selector seen, even with nth child odd or even!
-document.querySelector('li:nth-child(odd)').style.background = '#cccccc';
+// document.getElementsByTagName
+const lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+// items[3].textContent = 'Hello';
