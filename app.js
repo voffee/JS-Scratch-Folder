cd7983ae22caa702e236a2a9a338aa256f67dcb3
F Bliss <vanavil@fastmail.com>
2022-11-20 14:19:38 +0800

    DOM Multiple Selectors Complete

--- app.js
+++ app.js
@@ -12,8 +12,47 @@
 // console.log(listItems);
 
 // document.getElementsByTagName
-const lis = document.getElementsByTagName('li');
-console.log(lis);
-console.log(lis[0]);
-lis[0].style.color = 'red';
-// items[3].textContent = 'Hello';
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Hello';
+
+// Convert HTML Collection into array
+// lis = Array.from(lis);
+
+// Break down this piece of code and understand what it does 100%
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent = `${index}: Hello`;
+// })
+
+// lis.reverse();
+
+// console.log(lis);
+
+// document.querySelectorAll
+// outputs Node List, doesn't have to be converted to array
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item, index){
+    item.textContent = `${index}: Hello`;
+})
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(li, index){
+    li.style.background = '#cccccc';
+});
+
+console.log(liEven);
+
+
+// For loop will work with HTML collection and node list, since length still works on non arrays
+for(let i = 0; i < liEven.length; i++){
+    liEven[i].style.background = '#f4f4f4';
+}
+
+
+console.log(items);
